d79cd1ef7183bf938ae372b530b25f3179869a25
Liuzhonghao <1275718633@qq.com>
2023-05-11 14:55:38 +0800

    1111

--- springBoot-mybatis/src/test/java/com/example/springbootmybatis/SpringBootMybatisApplicationTests.java
+++ springBoot-mybatis/src/test/java/com/example/springbootmybatis/SpringBootMybatisApplicationTests.java
@@ -1,13 +1,34 @@
 package com.example.springbootmybatis;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 @SpringBootTest
 class SpringBootMybatisApplicationTests {
 
+
+    @Autowired
+    DataSource dataSource;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws SQLException {
+        System.out.println(dataSource.getClass());
+        Connection connection = dataSource.getConnection();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from user");
+        while(resultSet.next()){
+            System.out.println(resultSet.getInt("id")+"===="+resultSet.getString("name")+"======"+resultSet.getString("age"));
+        }
+        System.out.println(connection);
+        statement.close();
+        connection.close();
     }
 
 }
